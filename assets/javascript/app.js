a3f9d97fefa0c003258abd89c64b5332520826ab
Danielthweatt <dthweatt192@yahoo.com>
2018-04-15 16:20:11 -0500

    fixed file names, scaffolded html, worked on js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,6 +15,39 @@ firebase.initializeApp(config);
 
 // Variables
 
+const database = firebase.database();
+let isPlayerOne;
+let isPlayerTwo;
+let whoAmI;
+const gameStartButton = $("#gameStartButton");
+const gameDiv = $("#gameDiv");
+const statusDiv = $("#statusDiv");
+
 // Function Declarations
 
 // Function Calls
+
+database.ref("playerOne").on("value", function(snapshot){
+    isPlayerOne = snapshot.val();
+});
+
+database.ref("playerTwo").on("value", function(snapshot){
+    isPlayerTwo = snapshot.val();
+});
+
+gameStartButton.on("click", function(){
+    if (!isPlayerOne) {
+        database.ref("playerOne").set(true);
+        whoAmI = "playerOne";
+        statusDiv.append(`<h3 class="text-center">Player One</h3>`);
+        gameDiv.empty().append(`<p class="text-center">Waiting for Player Two to join.</p>`);
+    } else if (!isPlayerTwo) {
+        database.ref("playerTwo").set(true);
+        whoAmI = "playerTwo";
+        statusDiv.append(`<h3 class="text-center">Player Two</h3>`);
+        gameDiv.empty();
+    } else {
+        gameDiv.empty();
+        statusDiv.append(`<p class="text-center">Sorry! Two people are playing the game right now. No more than two players can play at one time.</p>`);
+    }
+});
