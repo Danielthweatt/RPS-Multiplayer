fb04627fa6b365e438fbeb028d01f6d3873959c5
Danielthweatt <dthweatt192@yahoo.com>
2018-04-15 15:20:36 -0500

    started scaffolding html, added reset css, linked to firebase, and started README

---
+++ README.md
@@ -0,0 +1,4 @@
+#Train Tracker
+This is my second application using firebase. It 
+is a Rock-Paper-Scissors Multiplayer game.
+##How to Use
---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Initialize Firebase
+
+const config = {
+    apiKey: "AIzaSyBVfamX-hyA3-F79t1kOdXzM9GuVlm1Zrs",
+    authDomain: "rps-multiplayer-c7ba8.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-c7ba8.firebaseio.com",
+    projectId: "rps-multiplayer-c7ba8",
+    storageBucket: "rps-multiplayer-c7ba8.appspot.com",
+    messagingSenderId: "240566913012"
+};
+
+firebase.initializeApp(config);
+
+// Variables
+
+// Function Declarations
+
+// Function Calls
